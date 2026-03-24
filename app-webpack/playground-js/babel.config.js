5d9dd88de3350c19c8309d362564a4e68434f657
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2026-03-24 19:52:44 +0200

    feat(app-webpack): add playground-js & playground-ts

---
+++ app-webpack/playground-js/babel.config.js
@@ -0,0 +1,10 @@
+export default api => ({
+  presets: [
+    [
+      '@quasar/babel-preset-app',
+      api.caller(caller => caller && caller.target === 'node')
+        ? { targets: { node: 'current' } }
+        : {}
+    ]
+  ]
+})
---
+++ app-webpack/playground-js/src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="https://cdn.quasar.dev/logo-v2/svg/logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
