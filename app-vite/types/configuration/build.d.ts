03b94794a22d5f7da14d82833e5f464aaf973780
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2026-03-31 14:31:50 +0300

    feat(app-vite): small tweak to quasar.config > build > define type

--- app-vite/types/configuration/build.d.ts
+++ app-vite/types/configuration/build.d.ts
@@ -353,7 +353,7 @@ interface QuasarStaticBuildConfiguration {
    * @example { __APP_VERSION__: JSON.stringify('v1.0.0') }
    * @example { __API_URL__: 'window.__backend_api_url' }
    */
-  define?: Record<string, any>;
+  define?: Record<string, string>;
 
   /**
    * Configuration related to the environment variables loaded from
