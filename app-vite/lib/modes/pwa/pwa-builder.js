03e3db6765c9e80402468f25dd76fab7458b052b
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2026-04-28 12:21:11 +0300

    chore(app-vite): small tweaks to PWA build

--- app-vite/lib/modes/pwa/pwa-builder.js
+++ app-vite/lib/modes/pwa/pwa-builder.js
@@ -9,8 +9,7 @@ export class QuasarModeBuilder extends AppBuilder {
   async build() {
     injectPwaManifest(this.quasarConf)
 
-    const viteConfig = await quasarPwaConfig.vite(this.quasarConf)
-    await this.buildWithVite('PWA UI', viteConfig)
+    await this.#buildUI()
 
     // also update ssr-builder.js when changing here
     writeFileSync(
@@ -23,6 +22,17 @@ export class QuasarModeBuilder extends AppBuilder {
       'utf8'
     )
 
+    await this.#buildPWA()
+
+    this.printSummary(this.quasarConf.build.distDir, true)
+  }
+
+  async #buildUI() {
+    const viteConfig = await quasarPwaConfig.vite(this.quasarConf)
+    await this.buildWithVite('PWA UI', viteConfig)
+  }
+
+  async #buildPWA() {
     // also update ssr-builder.js when changing here
     if (this.quasarConf.pwa.workboxMode === 'InjectManifest') {
       const rolldownConfig = await quasarPwaConfig.customSw(this.quasarConf)
@@ -32,7 +42,5 @@ export class QuasarModeBuilder extends AppBuilder {
     // also update ssr-builder.js when changing here
     const workboxConfig = await quasarPwaConfig.workbox(this.quasarConf)
     await buildPwaServiceWorker(this.quasarConf, workboxConfig)
-
-    this.printSummary(this.quasarConf.build.distDir, true)
   }
 }
