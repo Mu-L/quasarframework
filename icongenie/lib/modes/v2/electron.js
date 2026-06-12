eaa1fdbc390aefcc9e5d5a2f3442d97b32964bc6
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2026-06-12 15:45:16 +0300

    fix(icongenie): electron compat with q/app-vite v2 and q/app-webpack v4

--- icongenie/lib/modes/v2/electron.js
+++ icongenie/lib/modes/v2/electron.js
@@ -1,8 +1,8 @@
 import { existsSync } from 'node:fs'
 import { resolveDir } from '../../utils/app-paths.js'
 
-const dir = existsSync(resolveDir('src-electron/electron-assets'))
-  ? 'electron-assets' // q/app-vite v3+
+const dir = existsSync(resolveDir('src-electron/electron-assets/icons'))
+  ? 'electron-assets/icons' // q/app-vite v3+
   : existsSync(resolveDir('src-electron/icons'))
     ? 'icons' // q/app-vite v2 or q/app-webpack v4
     : 'electron-assets' // fallback to q/app-webpack v3 specs
@@ -12,21 +12,21 @@ export default [
     // macos (embedded icons)
     generator: 'icns',
     name: 'icon.icns',
-    folder: `src-electron/${dir}/icons`
+    folder: `src-electron/${dir}`
   },
 
   {
     // windows (embedded icon)
     generator: 'ico',
     name: 'icon.ico',
-    folder: `src-electron/${dir}/icons`
+    folder: `src-electron/${dir}`
   },
 
   {
     // tray icon (all platforms)
     generator: 'png',
     name: 'icon.png',
-    folder: `src-electron/${dir}/icons`,
+    folder: `src-electron/${dir}`,
     sizes: [512]
   }
 ]
